f66174131781c4567961d1df7adde2e1109b4229
michael <mavery735@gmail.com>
2023-02-28 00:20:14 -0500

    Keepass opens in keepass desktop

--- config.h
+++ config.h
@@ -27,7 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Keepassxc",	NULL,	NULL,	1 << 8,	0,	-1 },
+	{ "KeePassXC",	NULL,	NULL,	  1 << 8,	0,	-1 },
 };
 
 /* layout(s) */
