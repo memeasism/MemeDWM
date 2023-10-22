eb806729ff2b4ac3aeb492c108d7a8af63e54963
michael <mavery735@gmail.com>
2023-10-22 17:35:46 -0400

    made the top bar scale perfect!

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=15"};
-static const char dmenufont[]       = "monospace:size=15";
+static const char *fonts[]          = { "monospace:size=12"};
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#282828";
 static const char col_gray3[]       = "#686868";
