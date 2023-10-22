dbaead9698d0c4540f8baaa3ae07d4dd9cc5afcb
michael <mavery735@gmail.com>
2023-10-22 00:24:50 -0400

    missed one instance of col_cyan which was replaced with col_red because I didnt substitute in vim just thought there was two instances

--- config.h
+++ config.h
@@ -55,7 +55,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *browsercmd[] = { "librewolf", NULL};
 static const char *networkmanagercmd[] = { "nm-connection-editor", NULL };
