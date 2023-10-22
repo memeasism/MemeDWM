c1d7537c2a22aa4331fb434c17eeb46d3f5244a9
michael <mavery735@gmail.com>
2023-10-22 00:21:17 -0400

    made colors match the memeasism logo(bill nye the lawyer guy)

--- config.h
+++ config.h
@@ -7,15 +7,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#282828";
+static const char col_gray3[]       = "#686868";
+static const char col_gray4[]       = "#C0C0C0";
+static const char col_red[]        = "#E53B44";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_red,  col_red},
 };
 
 /* tagging */
