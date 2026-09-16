7fa04c32ddf9ba7dbf255c5393d138617ca03db6
Memeasism <82785402+memeasism@users.noreply.github.com>
2026-09-16 01:15:10 -0400

    Remove systray pinning and visibility settingsfix?
    Removed systray configuration options.

--- config.def.h
+++ config.def.h
@@ -5,8 +5,6 @@ static const char brdecrement[] = "-5%" ;
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12", "SymbolsNerdFont:size=12"};
